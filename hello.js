2e5b4ea6db1be4577c24323e28744dd9774d4a81
riaakter1998 <ria426306@gmail.com>
2023-03-08 23:17:23 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+var mydate = new Date();
+console.log(mydate.toTimeString());
+;
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toLocaleDateString());
+
+console.log(mydate.getMonth());
+console.log(mydate.getDate());
+console.log(mydate.getMinutes());
+console.log(mydate.setMilliseconds());
+console.log(mydate.getSecondsgit init());
